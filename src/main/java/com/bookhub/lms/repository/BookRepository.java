2bcf4926e566ee9175043f40ea31ae293a9a8839
rana-mamdouh <ranaammamdouh@gmail.com>
2024-05-19 10:15:17 +0300

    First Commit
    First Commit

--- src/main/java/com/bookhub/lms/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookhub.lms.repository;
-
-import com.bookhub.lms.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/com/bookhub/lms/repository/PatronRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookhub.lms.repository;
-
-import com.bookhub.lms.entity.Patron;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatronRepository extends JpaRepository<Patron, Long> {
-}
